1407af3cac1c47a394a631c833ac5711cff7f637
mhassaan <mhassaanchaudhry@gmail.com>
2018-10-17 21:37:39 +0200

    replace html image tags by rails image_tag helper.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,7 +47,10 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  #config.assets.debug = true
+  config.action_controller.asset_host = "localhost:3000"
+  config.assets.digest= false
+  #config.assets.compile = false
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
