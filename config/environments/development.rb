d2a6d51a964494f5ccd3f259d36bc8b9fb78cc90
mhassaan <mhassaanchaudhry@gmail.com>
2018-10-17 21:39:17 +0200

    Remove asset host settings.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,7 +48,7 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   #config.assets.debug = true
-  config.action_controller.asset_host = "localhost:3000"
+  #config.action_controller.asset_host = "localhost:3000"
   config.assets.digest= false
   #config.assets.compile = false
 
